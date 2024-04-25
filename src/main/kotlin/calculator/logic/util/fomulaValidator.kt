28a6a074bcf5df3c5b1d9c6c73b7eb3f769ed5e8
LSH <fhzktm@naver.com>
2024-04-25 14:57:42 +0900

    refactor : operationConverter 메서드, operand 와 operator 검증 메서드에 주석 추가

--- src/main/kotlin/calculator/logic/util/fomulaValidator.kt
+++ src/main/kotlin/calculator/logic/util/fomulaValidator.kt
@@ -2,6 +2,9 @@ package calculator.logic.util
 
 import calculator.logic.constants.OperatorEnum
 
+/**
+ * Verify that the argument is a valid operator.
+ * Throw IllegalArgumentException with the message "invalid operator: symbol" if it is not the symbol "+", "-", "*", "/%".*/
 fun String.validateIsOperator() = run {
     val operatorSymbols = OperatorEnum.Operator.values().map { it.symbol }
     if(this !in operatorSymbols){
@@ -9,6 +12,9 @@ fun String.validateIsOperator() = run {
     }
 }
 
+/**
+ * Verify that the argument is a valid operand.
+ * Throw NumberFormatException if it is not an integer string. */
 fun String.validateIsOperand()  = run {
     this.toInt()
 }
--- src/main/kotlin/calculator/logic/util/operationConverter.kt
+++ src/main/kotlin/calculator/logic/util/operationConverter.kt
@@ -3,6 +3,9 @@ package calculator.logic.util
 import calculator.logic.constants.OperatorEnum
 import calculator.logic.operation.*
 
+/**
+ * Returns the operator object according to the string entered as an argument.
+ * Throw IllegalArgumentException with the message "invalid operator: symbol" if it is not the symbol "+", "-", "*", "/%".*/
 fun String.convertOperation() = run {
     when (this) {
         OperatorEnum.Operator.ADDITION.symbol -> AddOperation()
